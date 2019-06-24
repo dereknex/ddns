eed5c5461a9eb055812767f1531e219bd4325d6f
Derek Chen <derek.amz@gmail.com>
2019-06-24 15:50:17 +0800

    add cli logic

--- cmd/client.go
+++ cmd/client.go
@@ -1,9 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/dereknex/ddns/pkg/v1/config"
+	"github.com/dereknex/ddns/pkg/v1/provider"
+	"github.com/dereknex/ddns/pkg/v1/solver"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -11,6 +17,7 @@ import (
 )
 
 var cfgFile string
+const duration = 5 * time.Second
 
 func init() {
 	rootCmd.AddCommand(clientCmd)
@@ -26,7 +33,12 @@ func initConfig() {
 		os.Exit(1)
 	}
 	viper.SetConfigType("yaml")
+
 	if err := viper.ReadInConfig(); err != nil {
+		logrus.Error("Can't read config: ", err)
+		os.Exit(1)
+	}
+	if err := viper.Unmarshal(config.ClientConfig); err != nil {
 		logrus.Error("Can't read config: ", err)
 		os.Exit(1)
 	}
@@ -53,12 +65,46 @@ func setupSignalHandler() <-chan struct{} {
 	return stop
 }
 
+func run() {
+	manager, err := solver.NewSolverManager(config.ClientConfig.Solvers)
+	if err != nil {
+		logrus.Error("Can not create solve manager: ", err)
+		os.Exit(1)
+	}
+	ticker := time.NewTicker(duration)
+	defer ticker.Stop()
+	for now := range ticker.C {
+		fmt.Printf("Working at %v\n", now)
+		ip, err := manager.Run()
+		if err != nil {
+			logrus.Error("Can not get IP address: ", err)
+		}
+		for k, d := range config.ClientConfig.Domains {
+			domain := strings.ReplaceAll(k, "#", ".")
+			p, err := provider.NewProvider(d.Provider)
+			if err != nil {
+				logrus.Error("Can not new domain provider: ", err)
+				continue
+			}
+			for _, entry := range d.Entrys {
+				err = p.SetRecord(entry, ip, domain)
+				if err != nil {
+					logrus.Error("Can not set IP: ", err)
+					continue
+				}
+			}
+		}
+
+	}
+}
+
 var clientCmd = &cobra.Command{
 	Use:   "client",
 	Short: "DDNS run in client mode",
 	Run: func(cmd *cobra.Command, args []string) {
-		stopCh := setupSignalHandler()
 
+		stopCh := setupSignalHandler()
+		go run()
 		//go daemon.SdNotify(false, "READY=1")
 
 		<-stopCh
--- pkg/v1/config/types.go
+++ pkg/v1/config/types.go
@@ -1,6 +1,23 @@
 package config
 
-type ClientConfig struct {
+import "sync"
+
+type DomainConfig struct {
+	Provider string `yaml:"provider"`
+	Entrys []string `yaml:"entrys"`
+}
+
+type clientConfig struct {
 	Solvers []string            `yaml:"solvers"`
-	Domains map[string][]string `yaml:"domains"`
+	Domains map[string]DomainConfig `yaml:"domains"`
+}
+
+
+var ClientConfig *clientConfig;
+var newConfig sync.Once
+
+func init(){
+	newConfig.Do(func() {
+		ClientConfig = &clientConfig{}
+	})
 }
